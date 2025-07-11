cde7ea3b969276ee205f69c9d8939b9354c2763c
ggambopce <ggambopce@gmail.com>
2025-07-11 22:00:10 +0900

    배열의 추가

--- src/collection/array/ArrayMain1.java
+++ src/collection/array/ArrayMain1.java
@@ -17,9 +17,19 @@ public class ArrayMain1 {
         arr[2] = 10;
         System.out.println(Arrays.toString(arr));
 
+        /**
+         * 배열은 메모리상에 순서대로 붙어서 존재한다.
+         * int는 4byte를 차지한다
+         * 시작 참조값에서 인덱스번호에 해당하는 자료의 크기를 곱하여 메모리 위치를 찾을 수 있다.
+         * 한번의 계산으로 위치를 찾을 수 있다.
+         */
         System.out.println("==index 조회: 0(1)==");
         System.out.println("arr[2] = " + arr[2]);
 
+        /**
+         * 배열안에 들어있는 데이터를 순회하면서 하나하나 확인해야한다.
+         * 검색의 경우 데이터 크기만큼 연산이 필요하다 크기가 n이면 연산도 n만큼 필요하다
+         */
         System.out.println("==배열 검색: 0(n)==");
         System.out.println(Arrays.toString(arr));
         int value = 10;
@@ -32,3 +42,11 @@ public class ArrayMain1 {
         }
     }
 }
+
+/**
+ * 빅오 표기법
+ * 알고리즘의 성능을 분석할 때 사용하는 수학적 표현방식
+ * 알고리즘의 적황한 실행 시간을 계산하는것이 아니라 데이터 양의 증가에 따른 성능의 변화 추세
+ * O(1) 최적 - 데이터 크기와 상관없이 일정
+ * O(n) 최악 - 데이터 크기가 크면 비례하여 시간이 증가 O(n/2)도 O(n)으로 볼수 있다.
+ */
---
+++ src/collection/array/ArrayMain2.java
@@ -0,0 +1,72 @@
+package collection.array;
+
+import java.util.Arrays;
+
+public class ArrayMain2 {
+    /**
+     * 배열의 추가
+     * 배열의 첫번째 추가
+     * 배열의 중간 위치 추가
+     * 배열의 마지막 위치 추가
+     * 공간을 확보하고 추가해야한다.
+     */
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        arr[0] = 1;
+        arr[1] = 2;
+        System.out.println(Arrays.toString(arr));
+
+        /**
+         * 배열의 첫번째 위치에 추가
+         * 기본 배열의 데이터를 한 칸씩 뒤로 밀고 배열의 첫번째 위치에 추가
+         * 위치 파악 O(1)
+         * 오른쪽 데이터 한칸씩 이동 O(n)
+         */
+        System.out.println("배열의 첫번째 위치에 3 추가 O(n)");
+        int newValue = 3;
+        addFirst(arr, newValue);
+        System.out.println(Arrays.toString(arr));
+
+        /**
+         * 배열의 가운데 위치에 추가
+         * 가운데 위치 파악하고 다음 데이터는 한칸씩 이동
+         * 위치 파악 0(1)
+         * 오른쪽 데이터 한칸씩 이동 O(n/2) -> O(n)
+         */
+        System.out.println("배열의 index(2) 위치에 4 추가 O(n)");
+        int index = 2;
+        int value = 4;
+        addAtIndex(arr, index, value);
+        System.out.println(Arrays.toString(arr));
+
+        /**
+         * 배열의 마지막 위치에 추가
+         * 배열이 이동하지 않고 바로 접근 가능
+         * 가장 빠르다 O(1)
+         */
+        System.out.println("배열의 마지막 위치에 5추가 O(1)");
+        addLast(arr, 5);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void addLast(int[] arr, int newValue ) {
+        arr[arr.length - 1] = newValue;
+    }
+
+    private static void addAtIndex(int[] arr, int index, int value) {
+        // 뒤에서 부터 인덱스까지
+        for (int i = arr.length-1; i > index; i--) {
+            arr[i] = arr[i - 1];
+        }
+        arr[index] = value;
+    }
+
+    public static void addFirst(int[] arr, int newValue) {
+        // 뒤에서 부터 순회하면서 오른쪽에서 왼쪽으로 값 이동
+        for (int i = arr.length-1; i > 0 ; i--) {
+            arr[i] = arr[i-1];
+        }
+        arr[0] = newValue;
+    }
+
+}
